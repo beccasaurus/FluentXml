acd675483fa5ee940d7db1a19aef4a07d24ffcf5
remi <remi@remitaylor.com>
2011-04-18 06:53:33 -0700

    0.1.0.1

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
 
 [assembly: AssemblyTitle("FluentXml")]
 [assembly: AssemblyProduct("FluentXml")]
